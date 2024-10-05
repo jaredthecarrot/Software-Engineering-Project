127091f089ac599f742a9b91be5b57fd49e49586
jaredthecarrot <jaredmccain.jam@gmail.com>
2024-10-04 20:49:46 -0500

    discussions logic, no integration yet

--- tamiu_social_media/urls.py
+++ tamiu_social_media/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('', include('posts.urls'))
+    path('', include('posts.urls')),
+    path('', include('discussions.urls'))
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
