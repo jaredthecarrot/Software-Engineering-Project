d4f61ef5173301ccd94c60ce3f56d4e7ab8bc521
angelggo <angelggo200401@gmail.com>
2024-11-26 10:14:28 -0600

    small image bug fix

--- messagingFeature/urls.py
+++ messagingFeature/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from . import views
 from uuid import UUID
+
 
 urlpatterns = [
     path('chat/<int:user_id>/', views.chat_view, name='chat_view')
