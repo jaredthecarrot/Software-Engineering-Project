c72d51a4249dcb72d619132dd2cd8062f990c55e
angelggo <angelggo200401@gmail.com>
2024-11-30 11:58:01 -0600

    fixing profile bug and adding migrations to have friends under profile

--- core/models.py
+++ core/models.py
@@ -8,8 +8,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default = 'blank_profile_picture.avif')
+    profileimg = models.ImageField(upload_to='profile_images', default='blank_profile_picture.avif')
     location = models.CharField(max_length=100, blank=True)
-    
+    friends = models.ManyToManyField('self', blank=True, symmetrical=True)
+
     def __str__(self):
         return self.user.username
